484af1d149a6ae1959cde8bacc38951734f5d508
ssSobuj <react.tork@gmail.com>
2024-12-16 12:16:31 +0600

    Added path alias config

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@core': path.resolve(__dirname, './src/core')
+    }
+  }
+})
